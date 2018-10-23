1a64cf69f1e236b6333834dff8b5c877e976ef7d
Hoàng Thắng <Hoangthang@MacBook-Pro-de-mac.local>
2018-10-24 00:44:22 +0700

    update code

--- stackover_flow/topic_detail.py
+++ stackover_flow/topic_detail.py
@@ -2,13 +2,30 @@ __author__ = 'Hoangthang'
 
 from bs4 import BeautifulSoup
 import requests
+import json
+
+def get_topic_detail(url):
+    result = {}
+    data = get_object_html(url)
+    answers = get_answers(data)
+    title_question = get_title_question(data)
+    content_question = get_content_question(data)
+    question_vote_count = get_vote_count_post(data)
+
+    if answers is not None and len(answers) != 0:
+        result['answers'] = answers
+        result['title_question'] = title_question
+        result['content_question'] = content_question
+        result['question_vote_count'] = question_vote_count
+        print json.dumps(result)
+    return result
 
 def get_object_html(url):
     response = requests.get(url=url)
     data = BeautifulSoup(response.text)
     return data
 
-def get_question(data):
+def get_title_question(data):
     #print data
     question = data.find('a', {'class': 'question-hyperlink'})
     return question.text
@@ -23,15 +40,18 @@ def get_content_question(data):
     content = []
     for elm in temp:
         if len(elm.findChildren()) == 0:
-            content.append({'tag name': elm.name, "content": elm.text})
+            content.append({'tag_name': elm.name, "content": elm.text})
     return content
 
 def get_answers(data):
+    answer_result = []
     content = data.find('div', {'id': 'answers'})
     answers = content.find_all('div', {'class': 'answer'})
-    print len(answers)
     for answer in answers:
-        get_answer(answer)
+        temp = get_answer(answer)
+        if temp is not None:
+            answer_result.append(temp)
+    return answer_result
 
 
 def get_answer(answer_content):
@@ -42,8 +62,9 @@ def get_answer(answer_content):
         temp = answer_content.find("div", {'class': 'post-text'}).findChildren()
         for elm in temp:
             if len(elm.findChildren()) == 0:
-                content.append({'tag name': elm.name, "content": elm.text})
+                content.append({'tag_name': elm.name, "content": elm.text})
         return {'user_comment': username, 'data': content, 'answered_time': _time}
+    return None
 
 def check_accepted_answer(div_content):
     answer_vote = div_content.find('div', {'class': 'vote'})
@@ -62,8 +83,10 @@ def get_time_comment(answer_content):
     answered_time = answer_content.find('div', {'class': 'user-action-time'})
     answered_time = answered_time.find('span')
     return answered_time.text
+
+
 url = 'https://stackoverflow.com/questions/49114011/how-to-check-out-a-pull-request-with-jenkins-pipeline'
+#data = get_object_html(url)
+#print get_answers(data)
 
-data = get_object_html(url)
-print get_answers(data)
-
+get_topic_detail(url)
---
+++ stackover_flow/topics.py
@@ -0,0 +1,81 @@
+__author__ = 'Hoangthang'
+
+import requests
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+import topic_detail as top_de
+from pymongo import MongoClient
+import traceback
+
+BASE_DOMAIN = 'https://stackoverflow.com'
+
+def run():
+    url = "https://stackoverflow.com/questions"
+    while(True):
+        print url
+        data = get_object_html(url)
+        get_questions(data)
+        next_pages = get_next_page(data)
+        if next_pages == -1:
+            return
+        url = 'https://stackoverflow.com/questions?page=' + str(next_pages) + '&sort=votes'
+
+
+def get_questions(data):
+    questions_object = data.find('div', {'id': 'questions'})
+    questions = questions_object.find_all('div', {'class': 'question-summary'})
+    print len(questions)
+    pool = ThreadPoolExecutor(max_workers=20)
+    for question in questions:
+        pool.submit(get_question_detail, question)
+    pool.shutdown(wait=True)
+
+def get_question_detail(question):
+    if check_question(question):
+        url = get_question_link(question)
+        top_de.get_topic_detail(url)
+
+def get_question_link(question):
+    a = question.find('a', {'class': 'question-hyperlink'})
+    return  BASE_DOMAIN + a['href']
+
+def check_question(question):
+    #print question
+    answered_accepted = question.find('div', {'class': 'answered-accepted'})
+    if answered_accepted is None:
+        return False
+    votes = question.find('div', {'class': 'votes'})
+    if votes is None:
+        return False
+    vote_count = votes.text
+    vote_count = vote_count.replace('votes', "").replace(' ', '')
+    if int(vote_count) == 0:
+        return False
+    return True
+
+def get_next_page(data):
+    pages = data.find('div', {'class': 'pager'})
+    current_pages = pages.find('span', {'class': 'current'})
+    print current_pages.text
+    if current_pages is None or int(current_pages.text) <= 0:
+        return -1
+    return int(current_pages.text) + 1
+
+
+def get_object_html(url):
+    response = requests.get(url=url)
+    data = BeautifulSoup(response.text)
+    return data
+
+def insert_mongodb(data):
+    mongo_client = MongoClient("localhost", 27017)
+    try:
+        db = mongo_client.get_database("stackover_flow")
+        collection = db.get_collection("questions")
+        collection.insert(data)
+    except:
+        traceback.print_exc()
+
+url = "https://stackoverflow.com/questions"
+data = get_object_html(url)
+get_questions(data)
